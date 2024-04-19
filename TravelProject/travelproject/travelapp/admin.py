9b17ccc0ecb4a84147ea061465915d45fee0218d
febinhusna <136878411+febinhusna@users.noreply.github.com>
2024-04-19 12:02:51 +0530

    Delete TravelProject directory

--- TravelProject/travelproject/travelapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Place , Men
-# Register your models here.
-admin.site.register(Place)
-admin.site.register(Men)
--- TravelProject/travelproject/travelapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Place, Men
-
-def demo(request):
-    obj = Place.objects.all()
-    men = Men.objects.all()
-    return render(request, "index.html", {'result':obj , 'men': men})
-
-
